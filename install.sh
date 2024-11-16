931855e68dc1ca28e0fe73427fe389a9e7de74bb
cheyao <94928179+cheyao@users.noreply.github.com>
2024-11-16 15:59:46 +0100

    More keys and fixes

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ln -sf `realpath external/zshrc` ~/.zshrc
-# cp -ri `realpath external/keyd` /etc/keyd
-ln -sf `realpath external/keyd` /etc/keyd
+ln -sf ~/.config/hypr/external/zshrc ~/.zshrc
+ln -sf ~/.config/hypr/external/ags ~/.config/ags
+sudo ln -sf ~/.config/hypr/external/keyd /etc/keyd
