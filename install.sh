d9c91633a490382b01c45357c468017070f3bd36
cheyao <94928179+cheyao@users.noreply.github.com>
2024-11-17 15:02:16 +0100

    More configs

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 ln -sf ~/.config/hypr/external/zshrc ~/.zshrc
 ln -sf ~/.config/hypr/external/kitty ~/.config/kitty
 ln -sf ~/.config/hypr/external/cava ~/.config/cava
+ln -sf ~/.config/hypr/external/waybar ~/.config/waybar
+ln -sf ~/.config/hypr/external/fcitx5 ~/.config/fcitx5
 sudo ln -sf ~/.config/hypr/external/keyd /etc/keyd
 
 for i in ~/.mozilla/firefox/*/; do ln -sf ~/.config/hypr/external/firefox/ $i/chrome; done
