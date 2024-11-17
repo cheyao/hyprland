54863c702146fe267d4a5b49ed47067cd3ba93bd
cheyao <94928179+cheyao@users.noreply.github.com>
2024-11-17 16:48:15 +0100

    Saber fastfetch

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -sf ~/.config/hypr/external/kitty ~/.config/kitty
 ln -sf ~/.config/hypr/external/cava ~/.config/cava
 ln -sf ~/.config/hypr/external/waybar ~/.config/waybar
 ln -sf ~/.config/hypr/external/fcitx5 ~/.config/fcitx5
+ln -sf ~/.config/hypr/external/fastfetch ~/.config/fastfetch
 sudo ln -sf ~/.config/hypr/external/keyd /etc/keyd
 
 for i in ~/.mozilla/firefox/*/; do ln -sf ~/.config/hypr/external/firefox/ $i/chrome; done
