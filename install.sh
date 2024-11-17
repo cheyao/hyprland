8edbd26fc3df333f3102610491d023e1cb5a33a2
cheyao <94928179+cheyao@users.noreply.github.com>
2024-11-17 14:16:33 +0100

    Kitty, cava & waybar

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 ln -sf ~/.config/hypr/external/zshrc ~/.zshrc
-ln -sf ~/.config/hypr/external/ags ~/.config/ags
+ln -sf ~/.config/hypr/external/kitty ~/.config/kitty
+ln -sf ~/.config/hypr/external/cava ~/.config/cava
 sudo ln -sf ~/.config/hypr/external/keyd /etc/keyd
+
+for i in ~/.mozilla/firefox/*/; do ln -sf ~/.config/hypr/external/firefox/ $i/chrome; done
